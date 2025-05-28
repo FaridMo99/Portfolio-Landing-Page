3fe0b3ef89e538e8545aaa0cedf9d9844f8a295d
FaridMo99 <farid-ahmad@hotmail.de>
2025-05-29 00:44:58 +0200

    added images to spa project and fade animation on hover

--- src/components/ProjectsCard.jsx
+++ src/components/ProjectsCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import { FaGithub } from "react-icons/fa";
 import CardButton from "./CardButton";
 import TransitionWrapper from "./HoC/TransitionWrapper";
@@ -7,27 +7,48 @@ function ProjectsCard({
   title,
   text,
   image,
+  secondImage,
   alt,
   githubLink,
   technologies,
   width,
+  link
 }) {
+  const [isHovered, setIsHovered] = useState(false)
+
   return (
+    <a href={link} target="_blank" onMouseEnter={()=>setIsHovered(true)} onMouseLeave={()=>setIsHovered(false)}>
     <TransitionWrapper
       styles={`${width} h-[60vh] md:h-[70vh] relative rounded-2xl frost`}
     >
-      <img className="h-1/3 w-full" src={image} alt={alt} />
-      <h2 className="hover:text-secondary pl-3 py-4 font-bold">{title}</h2>
+    <div className="relative h-1/3 w-full bg-black">
+  <img
+    src={image}
+    alt={alt}
+    className={`absolute h-full w-full transition-opacity duration-500 ${
+      isHovered ? "opacity-0" : "opacity-100"
+    }`}
+  />
+  <img
+    src={secondImage}
+    alt={alt}
+    className={`absolute h-full w-full  transition-opacity duration-500 ${
+      isHovered ? "opacity-100" : "opacity-0"
+    }`}
+  />
+</div>
+      <h2 className="text-white pl-3 py-4 font-bold">{title}</h2>
       <p className="pl-3 mb-4 overflow-scroll h-[20vh]">{text}</p>
       <div className="w-full pl-1 pb-10 flex flex-wrap">
         {technologies?.map((technologie) => (
           <CardButton key={technologie} text={technologie} />
         ))}
       </div>
-      <a className="absolute bottom-1 right-1" href={githubLink}>
+      <a className="absolute bottom-1 right-1" target="_blank" href={githubLink}>
         <FaGithub size={30} color="#000" />
       </a>
     </TransitionWrapper>
+    </a>
   );
 }
 
--- src/components/main/Projects.jsx
+++ src/components/main/Projects.jsx
@@ -7,12 +7,14 @@ function Projects() {
     <Subsections title="Projects" id="projects">
       <div className="flex md:flex-row flex-col justify-around md:justify-between items-center w-full h-full">
         <ProjectsCard
-          title="Social-Media-Clone/SPA"
-          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi fuga cupiditate recusandae dolorum porro vel, eaque tempore corporis modi commodi, repudiandae mollitia quam eos corrupti iure aperiam maxime explicabo itaque."
-          image="/self.jpeg"
-          githubLink="" 
+          title="friendly. /Social Media Single-Page-Application"
+          text="friendl. is a Social Media App offering CRUD functionality aswell as Authentication and Login/Signup logic."
+          image="/friendly-logo.png"
+          githubLink="https://github.com/FaridMo99/SPA" 
           technologies={["React", "React-Router", "Tailwind"]}
           width="w-full md:w-[25vw]"
+          link=""
+          secondImage="/friendly-logo-second.png"
         />
         <ProjectsCard
           title="Basketball-API/AI-Wrapper/SSR"
@@ -21,6 +23,9 @@ function Projects() {
           githubLink=""
           technologies={["React", "NextJs", "Tailwind"]}
           width="w-full md:w-[25vw] my-12 md:my-0"
+          link=""
+          secondImage=""
+
         />
         <ProjectsCard
           title="Finance-Dashboard/SSG"
@@ -29,6 +34,9 @@ function Projects() {
           githubLink=""
           technologies={["React", "NextJs", "Tailwind"]}
           width="w-full md:w-[25vw]"
+          link=""
+          secondImage=""
+
         />
       </div>
     </Subsections>
